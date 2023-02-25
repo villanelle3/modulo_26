cc2195d44ca5c5b932a95fbdac3bed34614e024c
Laura Augusta <laurah.algusta@gmail.com>
2023-02-25 02:18:05 -0300

    new files.

---
+++ exercicio.js
@@ -0,0 +1,8 @@
+"use strict";
+// npx tsc
+function multiplica(a, b) {
+    return a * b;
+}
+function ola(nome) {
+    return `Olá, ${nome}`;
+}
---
+++ exercicio.ts
@@ -0,0 +1,9 @@
+// npx tsc
+
+function multiplica(a: number, b: number) {
+    return a * b;
+}
+
+function ola(nome: string) {
+    return `Olá, ${nome}`
+}
